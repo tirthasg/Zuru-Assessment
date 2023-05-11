cea6c02a58717f973fc9f331aa9f5603aa1e1e80
Tirtha <tirthasg@gmail.com>
2023-05-11 23:34:11 +0530

    additional comments, simplification of equations, and other minor changes. Awaiting clarification from domain experts

--- driver.py
+++ driver.py
@@ -36,42 +36,71 @@ def take_input():
 def beam_verification(data):
     # Assigning references to inputs of interest
     # Compressive strength of concrete
+    # Unit: MPa
     f_c = data[0]
+
     # Yield strength of steel
+    # Unit: MPa
     fy = data[1]
+
     # Max concrete strain
+    # Unit-less
     ep_cmax = data[2]
+
     # Modulus of elasticity of steel
+    # Unit: MPa
     E_s = data[3]
+
     # Strength reduction factor for Flexure
+    # Unit-less
     phi = data[4]
+
     # Width of beam
+    # Unit: mm
     bw = data[9]
+
     # Overall depth of the section
+    # Unit: mm
     h = data[10]
+
     # Effective cover (to center of bottom longitudinal reinforcement)
+    # Unit: mm
     covBott = data[11]
+
     # Effective cover (to center of top longitudinal reinforcement)
+    # Unit: mm
     covTop = data[12]
+
     # Bottom main reinforcement
+    # Unit: mm^2
     As_bott_prov = data[13]
+
     # Top main reinforcement
+    # Unit: mm^2
     As_top_prov = data[14]
+
     # Moment imported from analysis result
+    # Unit: kN-m
     M = data[15]
 
 
 
     # Beginning of main computation block
     # Effective depth of section (from top)
+    # Unit: mm
     dFromTop = h - covBott
+    
     # Effective depth of section (from bottom)
+    # Unit: mm
     dFromBott = h - covTop
+
     # Effective depth consideration based on design moment
+    # Unit: mm
     d = dFromBott if M < 0 else dFromTop
 
     # Width factor of equivalent rectangular concrete compressive stress distribution
-    alpha1 = None
+    # Unit-less
+    alpha1 = 0
     if f_c <= 55:
         alpha1 = 0.85
     elif 0.85 - 0.004 * (f_c - 55) < 0.75:
@@ -80,7 +109,8 @@ def beam_verification(data):
         alpha1 = 0.85 - 0.004 * (f_c - 55)
 
     # Height factor of equivalent rectangular concrete compressive stress distribution
-    beta1 = None
+    # Unit-less
+    beta1 = 0
     if f_c <= 30:
         beta1 = 0.85
     elif 0.85 - 0.008 * (f_c - 30) < 0.85:
@@ -89,27 +119,40 @@ def beam_verification(data):
         beta1 = 0.65
 
     # Depth of actual neutral axis from top
+    # Unit: mm
     c_actual_fromTop = (fy * As_bott_prov) / (bw * beta1 * alpha1 * f_c)
     # Depth of actual neutral axis from bottom
+    # Unit: mm
     c_actual_fromBott = (fy * As_top_prov) / (bw * beta1 * alpha1 * f_c)
 
     # Depth of equivalent actual rectangular concrete stress block from top
+    # Unit: mm
     a_FromTop = c_actual_fromTop * beta1
     # Depth of equivalent actual rectangular concrete stress block from bottom
+    # Unit: mm
     a_FromBott = c_actual_fromBott * beta1
 
     # Balanced depth of neutral axis from top, cb
-    cb_fromTop = (ep_cmax / (ep_cmax + fy / E_s)) * dFromTop
+    # Unit: mm
+    cb_fromTop = (ep_cmax / (ep_cmax + (fy / E_s))) * dFromTop
     # Balanced depth of neutral axis from bottom, cb
-    cb_fromBott = (ep_cmax / (ep_cmax + fy / E_s)) * dFromBott
+    # Unit: mm
+    cb_fromBott = (ep_cmax / (ep_cmax + (fy / E_s))) * dFromBott
 
     # Maximum allowed depth of equivalent rectangular stress block from top
+    # Unit: mm
     a_max_fromTop = 0.75 * beta1 * cb_fromTop
     # Maximum allowed depth of equivalent rectangular stress block from bottom
+    # Unit: mm
     a_max_fromBott = 0.75 * beta1 * cb_fromBott
 
     # Design Moment for section
-    M_design = abs(M) * 10000
+    # Unit: N-m
+    M_design = abs(M) * 1000
+
+    # paramters = [f_c, fy, bw, covTop, covBott, dFromTop, dFromBott, As_bott_prov, As_top_prov, alpha1, beta1, a_FromTop, a_FromBott, a_max_fromTop, a_max_fromBott]
+    # parameters_for_moment = dict.fromkeys(paramters)
+    # print(parameters_for_moment)
 
     # Initializing class attributes
     Moment.init_class_attributes(f_c, fy, 
@@ -118,23 +161,29 @@ def beam_verification(data):
                                 dFromTop, dFromBott)
 
     # Creating an instance/object of the custom-class Moment
+    # obj = Moment(alpha1, 
+    #             dFromTop, a_FromBott, 
+    #             a_max_fromTop, a_max_fromBott)
     obj = Moment(alpha1, 
-                dFromTop, a_FromBott, 
-                a_max_fromTop, a_max_fromBott)
+                 a_FromTop, a_FromBott, 
+                 a_max_fromTop, a_max_fromBott)
 
     # Calling the instance method moment_capacity() to calculate the two moment capacities
+    # Unit: N-mm
     M_cap_Sagg, M_cap_Hogg = obj.moment_capacity()
     # Displaying the two moment capacities for Sagging & Hogging movement
-    # print('{:.4e}'.format(M_cap_Sagg))
-    # print('{:.4e}'.format(M_cap_Hogg))
+    print('{:.4e}'.format(M_cap_Sagg))
+    print('{:.4e}'.format(M_cap_Hogg))
 
     # Computing the Moment capacity
+    # Unit: N-mm
     M_cap = M_cap_Hogg * phi if M < 0 else M_cap_Sagg * phi
     # Displaying the Moment capacity
-    # print('{:.4e}'.format(M_cap))
+    print('{:.4e}'.format(M_cap))
 
     # Computing the result: Safety/Utilization factor
-    utilityRatioForFlexure = (M_design / M_cap) * 100
+    # Unit-less
+    utilityRatioForFlexure = (M_design / M_cap) * 1000
 
     # Return the result
     return utilityRatioForFlexure
--- moment.py
+++ moment.py
@@ -2,18 +2,29 @@
 class Moment:
     # Constructor to initialize the instance
     def __init__(self,
-                 alpha,
+                 alpha1,
                  a_fromTop, a_fromBott,
                  a_max_fromTop, a_max_fromBott):
 
         # All instance attributes are being stored as public 
-        self.alpha1 = 0.85
+        self.alpha1 = alpha1
 
-        self.a_fromTop = 160.8235
-        self.a_fromBott = 30.8824
+        self.a_fromTop = a_fromTop
+        self.a_fromBott = a_fromBott
 
-        self.a_max_fromTop = 113.3647
-        self.a_max_fromBott = 113.3647
+        self.a_max_fromTop = a_max_fromTop
+        self.a_max_fromBott = a_max_fromBott
+
+        # print("Important:")
+        # print(alpha1, a_fromTop, a_fromBott, a_max_fromTop, a_max_fromBott)
+
+        # self.alpha1 = 0.85
+
+        # self.a_fromTop = 160.8235
+        # self.a_fromBott = 30.8824
+
+        # self.a_max_fromTop = 113.3647
+        # self.a_max_fromBott = 113.3647
 
     # Class Method to initialize the class attributes
     @classmethod
@@ -23,18 +34,21 @@ class Moment:
                               covTop, covBott, As_top_prov, As_bott_prov, 
                               dFromTop, dFromBott):
         # All class attributes are being stored as public 
-        cls.f_c = 45
-        cls.fy = 675
+        cls.f_c = f_c
+        cls.fy = fy
 
-        cls.bw = 300
+        cls.bw = bw
 
-        cls.covTop = 60
-        cls.covBott = 60
-        cls.As_bott_prov = 2734
-        cls.As_top_prov = 525
+        cls.covTop = covTop
+        cls.covBott = covBott
+        cls.As_bott_prov = As_bott_prov
+        cls.As_top_prov = As_top_prov
 
-        cls.dFromTop = 440
-        cls.dFromBott = 440
+        cls.dFromTop = dFromTop
+        cls.dFromBott = dFromBott
+
+        # print("Another important:")
+        # print(f_c, fy, bw, covTop, covBott, As_bott_prov, As_top_prov, dFromTop, dFromBott)
 
     # Non-public Instance method to compute moment capacities
     def _M_cap_sing(cls, As, fy, d, a):
@@ -43,7 +57,9 @@ class Moment:
     # Non-public Instance method to compute moment capacities
     def _M_cap_doub(cls, alpha1, f_c, bw, a_max, d, fy, cov, As):
         return (alpha1 * f_c * bw * a_max * (d - a_max / 2)) + \
-               ((As - (alpha1 * f_c * bw * a_max * (d - a_max / 2)) / (fy * (d -  a_max / 2))) * (fy * (d - cov)))
+               (As - ((alpha1 * f_c * bw * a_max) / fy)) * fy * (d - cov)
+            #    (As - ((alpha1 * f_c * bw * a_max * (d - (a_max / 2))) / (fy * (d - (a_max / 2))))) * fy * (d - cov)
+            #    ((As - (alpha1 * f_c * bw * a_max * (d - a_max / 2)) / (fy * (d -  a_max / 2))) * (fy * (d - cov)))
 
     # Instance method to calculate the two moment capacities for Sagging & Hogging movement
     def moment_capacity(self):
@@ -55,15 +71,17 @@ class Moment:
         BeamDesignType_forsagging = "SRS" if self.a_fromTop <= self.a_max_fromTop else "DRS"
         BeamDesignType_forHogging = "SRS" if self.a_fromBott <= self.a_max_fromBott else "DRS"
 
+        print(BeamDesignType_forsagging, BeamDesignType_forHogging)
+
         # Computing the Sagging capacity based on Beam design type for Sagging movement
-        M_cap_Sagg = None 
+        M_cap_Sagg = 0 
         if BeamDesignType_forsagging == "SRS":
             M_cap_Sagg = self._M_cap_sing(Moment.As_bott_prov, Moment.fy, Moment.dFromTop, self.a_fromTop) 
         else:
             M_cap_Sagg = self._M_cap_doub(self.alpha1, Moment.f_c, Moment.bw, self.a_max_fromTop, Moment.dFromTop, Moment.fy, Moment.covTop, Moment.As_bott_prov)
 
         # Computing the Hogging capacity based on Beam design type for Hogging movement
-        M_cap_Hogg = None
+        M_cap_Hogg = 0
         if BeamDesignType_forHogging == "DRS":
             M_cap_Hogg = self._M_cap_sing(Moment.As_top_prov, Moment.fy, Moment.dFromBott, self.a_fromBott)
         else:
